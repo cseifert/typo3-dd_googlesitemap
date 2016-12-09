02798b3f39fc33e20a9ef33f6f0c0da81fe72d0b
Christian Seifert <christian-f-seifert@gmx.de>
2016-12-09 13:50:42 +0100

    [TASK] Raise dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,12 +27,12 @@ $EM_CONF[$_EXTKEY] = array (
   'clearCacheOnLoad' => 0,
   'lockType' => '',
   'author_company' => 'SIA "ACCIO"',
-  'version' => '2.1.1',
+  'version' => '2.1.2',
   'constraints' => 
   array (
     'depends' => 
     array (
-      'typo3' => '6.2.0-7.6.999',
+      'typo3' => '6.2.0-8.4.999',
     ),
     'conflicts' => 
     array (
